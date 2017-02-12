acf6be329a2386a6e52259380a5b67be191ffb3a
MatthewMawby <themesta@gmail.com>
2017-02-12 17:59:29 -0500

    added facebook auth logic to usercontroller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,6 +4,7 @@
  * @description :: Server-side logic for managing Users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var passport = require('passport');
 
 module.exports = {
   //ACTION NAME
@@ -15,8 +16,12 @@ module.exports = {
       req.session.user = null,
       req.session.flash = 'Logged out successfully';
       res.redirect('user/login');
-  }
-  
+  },
+
+  dashboard: function(req, res){
+      res.view();
+  },
+
   create: function(req, res) {
     //This is what the action will do
     //Creates a User in the database based on
@@ -32,4 +37,27 @@ module.exports = {
         return (err ? res.json(400, err): res.json(202,created) );
       });
   },
+
+  'facebook' : function(req, res, next){
+      passport.authenticate('facebook', {scope: ['email', 'user_about_me']},
+          function(err, user){
+              req.logIn(user, functino(err){
+                  if(err){
+                      req.session.flash = 'Error';
+                      res.redirect('user/login');
+                  }
+                  else{
+                      req.session.user = user;
+                      res.redirect('/user/dashboard');
+                  }
+              });
+          })(req, res, next);
+     },
+
+     'facebook/callback': function(req, res, next){
+         passport.authenticate('facebook',
+            function(req, res){
+                res.redirect('/user/dashboard');
+            })(req, res, next);
+     }
 };
